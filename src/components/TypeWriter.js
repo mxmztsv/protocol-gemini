c68e203f2c76f322c7640c5100be42fce53bb8b8
RussianHustla <maximilyn10@mail.ru>
2022-05-13 01:24:10 +0300

    main title animation changed

--- src/components/TypeWriter.js
+++ src/components/TypeWriter.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from "react"
 export const TypeWriter = ({ strings = [] }) => {
 
     const [displayedText, setDisplayedText] = useState("")
+    const [isAnimationIsDone, setIsAnimationIsDone] = useState(false);
 
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
@@ -18,35 +19,58 @@ export const TypeWriter = ({ strings = [] }) => {
 
     const typeStrings = async () => {
         // const reversedStrings = strings
-        while (true) {
+        // while (true) {
             for (const element of strings) {
                 await typeString(element)
                 await sleep(2000)
             }
-        }
+        // }
 
     }
 
     useEffect(async () => {
         await sleep(1000)
-        await typeStrings()
+        // await typeStrings()
+        typeStrings().then(async () => {
+            await sleep(2000)
+            setIsAnimationIsDone(true)
+        })
     }, []);
 
 
     return (
         <div className="typewriter">
-            <div className="typewriter__string">
-                <p className="typewriter__string__text">
-                    {displayedText}
-                </p>
-                <p className="typewriter__string__cursor">
-                    l
-                </p>
-            </div>
+            {/*<div className="typewriter__string">*/}
+            {/*    <p className="typewriter__string__text">*/}
+            {/*        {displayedText}*/}
+            {/*    </p>*/}
+            {/*    <p className="typewriter__string__cursor">*/}
+            {/*        l*/}
+            {/*    </p>*/}
+            {/*</div>*/}
+            {
+                isAnimationIsDone ? (
+                    <p className="typewriter__string__text glitch" data-text="YOUR FUTURE... HAS ARRIVED">
+                        Your Future... has arrived
+                    </p>
+                ) : (
+                    <>
+                        <div className="typewriter__string">
+                            <p className="typewriter__string__text">
+                                {displayedText}
+                            </p>
+                            <p className="typewriter__string__cursor">
+                                l
+                            </p>
+                        </div>
+                        <p className="typewriter__string__text blink">
+                            Your Future...
+                        </p>
+                    </>
 
-            <p className="typewriter__string__text">
-                Your Future...
-            </p>
+                )
+            }
+
         </div>
     )
 }
